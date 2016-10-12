c14872ab739c4d93ec7d88b9144cbac8d595b509
joaogodinho <joaocfgodinho@gmail.com>
2016-10-12 16:59:06 +0100

    Project skeleton

---
+++ dad-project/PuppetMaster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dad_project
+{
+    class PuppetMaster
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
